43ce567990f7e3e926b2972f33807005cb1e7c2e
Troy Carson <tcarson@hawaii.edu>
2017-10-10 23:27:08 -1000

    Task 5: Security with methods, Filtering data with publish and subscribe

--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -63,3 +63,16 @@ Template.body.helpers({
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
 });
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  // Only publish tasks that are public or belong to the current user
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId },
+      ],
+    });
+  });
+}
