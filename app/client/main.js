cb57fa1a7b0dd1c78d1acca743fd0ea0e20aefbc
Troy Carson <tcarson@hawaii.edu>
2017-10-10 23:25:42 -1000

    Task 3: Collections, Forms and events, update and remove

--- app/client/main.js
+++ app/client/main.js
@@ -1,1 +1,11 @@
-import '../imports/ui/body.js';
+import { Template } from 'meteor/templating';
+
+import { Tasks } from '../api/tasks.js';
+
+import './body.html';
+
+Template.body.helpers({
+  tasks() {
+    return Tasks.find({});
+  },
+});
--- app/imports/ui/body.js
+++ app/imports/ui/body.js
@@ -1,11 +1,65 @@
+return Tasks.find({});
+},
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
+});
+
 import { Template } from 'meteor/templating';
 
+import { Tasks } from '../api/tasks.js';
+
+import './task.html';
+
+Template.task.events({
+  'click .toggle-checked'() {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this._id, {
+      $set: { checked: ! this.checked },
+    });
+  },
+  'click .delete'() {
+    Tasks.remove(this._id);
+  },
+});
+
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+import { Tasks } from '../api/tasks.js';
+
+import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
+  tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
 });
